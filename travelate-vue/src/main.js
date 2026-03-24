1da91619cc759e50e2eaa030bfad1585b537903a
yenOmld <2971549832@qq.com>
2026-03-24 22:01:55 +0800

    Initial commit: YayFolk project

--- travelate-vue/src/main.js
+++ travelate-vue/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
 import Notification from './components/Notification.vue'
+import 'boxicons/css/boxicons.min.css'
 
 const app = createApp(App)
 
